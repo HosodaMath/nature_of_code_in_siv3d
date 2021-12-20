0eb58a09b150c729dd1063af08a89974360404fa
ShingoHosoda <tomas0ark@gmail.com>
2021-12-20 21:24:58 +0900

    add ch2_4_ch2_5_ch2_6_ch_2_7

---
+++ chapter2Forces/ch2_6_Gravity_on_Earth_and_Modeling_a_Force/Main.cpp
@@ -0,0 +1,40 @@
+﻿#include <Siv3D.hpp> 
+#include "Mover.hpp";
+
+void Main()
+{
+
+	Scene::SetBackground(ColorF{ 0.0, 0.0, 0.0 });
+
+	const int32 particleMAX = 10;
+	const int32 width = Scene::Width();
+	const int32 height = Scene::Height();
+	const Point windowSize = Point(width, height);
+	
+
+
+	std::vector<Mover> movers(particleMAX);
+	for (int32 count = 0; count < particleMAX; count++) {
+		const Vec2 position = Vec2(0, 0);
+		const double mass = Random<double>(5.0, 20.0);
+		Mover mover = Mover(position, mass);
+		movers.push_back(mover);
+	}
+
+	std::cout << movers.size() << std::endl;
+
+	while (System::Update())
+	{
+		for (int32 count = 0; count < movers.size(); count++) {
+			const double mass = movers[count].mass;
+			const Vec2 wind = Vec2(0.05, 0.0);
+			const Vec2 gravity = Vec2(0.0, 0.1 * mass);
+			movers[count].applyForce(wind);
+			movers[count].applyForce(gravity);
+			movers[count].update();
+			movers[count].render();
+			movers[count].checkEdge(windowSize);
+		}
+	}
+}
+
---
+++ chapter2Forces/ch2_7_Friction/Mover.hpp
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include <Siv3D.hpp> 
+
+class Mover {
+private:
+	Vec2 position;
+	Vec2 acceleration;
+public:
+	Vec2 velocity;
+	double mass;
+	Mover() = default;
+	Mover(const Vec2& position, const double& mass) : position(position), velocity(Vec2(0.0, 0.0)), acceleration(Vec2(0.0, 0.0)), mass(mass) {}
+
+
+	void applyForce(const Vec2& force) {
+		Vec2 calcForce = force / mass;
+		acceleration += calcForce;
+	}
+
+	void update() {
+		velocity += acceleration;
+		position += velocity;
+		acceleration *= 0.0;
+	}
+
+	void render() {
+		double scale = 2.0;
+		double size = mass * scale;
+		const ColorF color = ColorF{ 1.0, 1.0, 1.0, 1.0 };
+		Circle{ position, size }.draw(color);
+	}
+
+	void checkEdge(const Point& windowSize) {
+		if (position.x < 0) {
+			position.x = 0;
+			velocity.x *= -1;
+		}
+		else if (position.x > windowSize.x) {
+			position.x = windowSize.x;
+			velocity.x *= -1;
+		}
+
+		if (position.y < 0) {
+			position.y = 0;
+			velocity.y *= -1;
+		}
+		else if (position.y > windowSize.y) {
+			position.y = windowSize.y;
+			velocity.y *= -1;
+		}
+	}
+};
